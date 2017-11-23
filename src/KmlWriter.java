e2a0134fa0e7516d85d59c3ea47128d938bca684
user154 <user154@gmail.com>
2017-11-23 17:58:46 +0200

    Added Wifi icons according to Signal.

--- src/KmlWriter.java
+++ src/KmlWriter.java
@@ -11,36 +11,80 @@ public class KmlWriter {
 
 
 	/**
+	 * This class create Kml file from requested CSV.
+	 * Using JAK library.
 	 * @param LinkedList
 	 * @return 
 	 * @throws FileNotFoundException
 	 */
-	private LinkedList<Wifi> list;
-
-	public KmlWriter(LinkedList<Wifi> list){
+	private LinkedList<Wifi> list; 
+	/**Constructor for KmlWriter class.
+	 * @param list
+	 */
+	public KmlWriter(LinkedList<Wifi> list,String INI){
 		this.list = list;
-		KMLWriter(list);
+		KMLWriter(list , INITIAL.getWritePathForKML());
 	}
-	public void KMLWriter(LinkedList<Wifi> ls) {
+	/**Method that create document with all the points needed.
+	 * Get LinkedList.
+	 * @param ls
+	 */
+
+	public void KMLWriter(LinkedList<Wifi> ls,String WritePath) {
 		Kml kml = new Kml();
-		Document d = kml.createAndSetDocument();
+		Document d = kml.createAndSetDocument().withName("My Locations");
+		d.createAndSetTimeSpan().setBegin(ls.getFirst().StringToTime());
+		d.createAndSetTimeSpan().setEnd(ls.getLast().StringToTime());
+		Placemark p = null;
+		
+		Icon CellSignal = new Icon().withId("CellSignal");
+		CellSignal.setHref("/Users/gal/git/Wifi_Project/Icons/Cellular.png");
+		
+		Icon FullSignal = new Icon().withId("FullSignal");
+		FullSignal.setHref("/Users/gal/git/Wifi_Project/Icons/wifi-Full.png");
+		
+		Icon ThreeBarsSignal = new Icon().withId("3Signal");
+		ThreeBarsSignal.setHref("/Users/gal/git/Wifi_Project/Icons/wifi-3-bars.png");
+		
+		Icon TwoBarsSignal = new Icon().withId("2Signal");
+		TwoBarsSignal.setHref("/Users/gal/git/Wifi_Project/Icons/wifi-2-bars.png");
+		
+		Icon OneBarSignal = new Icon().withId("1Signal");
+		OneBarSignal.setHref("/Users/gal/git/Wifi_Project/Icons/wifi-1-bar.png");
+		
+		Icon EmptySignal = new Icon().withId("NoSignal");
+		EmptySignal.setHref("/Users/gal/git/Wifi_Project/Icons/wifi-Empty.png");
 		for(int i=0; i<ls.size(); i++)
 		{
-			d.createAndSetTimeSpan();
-			d.createAndAddPlacemark()
-			.withName(ls.get(i).getSSID())
-			.withDescription("\n "+ls.get(i).ShortToString())
-			.createAndSetPoint().addToCoordinates(
-					Double.parseDouble(ls.get(i).getLON()),
-					Double.parseDouble(ls.get(i).getLAT()), 
-					Double.parseDouble(ls.get(i).getALT())
-					);
+			p = kml.createAndSetPlacemark();
+			p.setName(ls.get(i).getSSID());
+			p.setDescription("\n "+ls.get(i).ShortToString());
+			p.createAndSetTimeSpan().withBegin(ls.get(i).StringToTime()).withEnd(ls.get(i).StringToTime());
+			if(ls.get(i).getRssiInINT() < 70){//weakest
+				p.createAndAddStyle().createAndSetIconStyle().withScale(0.6).withIcon(EmptySignal);				
+			}
+			else if(ls.get(i).getRssiInINT() >= 70 && ls.get(i).getRssiInINT() < 80){//weak
+				p.createAndAddStyle().createAndSetIconStyle().withScale(0.8).withIcon(ThreeBarsSignal);
+			}
+			else if(ls.get(i).getRssiInINT()>= 80 && ls.get(i).getRssiInINT() < 86){//normal
+				p.createAndAddStyle().createAndSetIconStyle().withScale(1.0).withIcon(TwoBarsSignal);
+			}
+			else if(ls.get(i).getRssiInINT()>= 86 && ls.get(i).getRssiInINT() < 100){//Good
+				p.createAndAddStyle().createAndSetIconStyle().withScale(1.2).withIcon(FullSignal);
+			}
+			else if(ls.get(i).getRssiInINT() >= 100){//Cellular
+				p.createAndAddStyle().createAndSetIconStyle().withScale(0.45).withIcon(CellSignal);
+			}
+			p.createAndSetPoint()
+			.addToCoordinates(Double.parseDouble(ls.get(i).getLON()), Double.parseDouble(ls.get(i).getLAT()), Double.parseDouble(ls.get(i).getALT()));
+			d.addToFeature(p);
 		}
 		//marshals to console
-		kml.marshal();
+		kml.setFeature(d);
+//		kml.marshal();
 		//marshals into file
 		try {
-			kml.marshal(new File("/Users/gal/Desktop/Kml.kml"));
+			kml.marshal(new File(WritePath));
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}	
